7a2e90a62b4bcd1cf297ea4a316d9657230c7323
dkoger44 <dbkr222@gmail.com>
2021-05-25 14:39:31 -0400

    changing background color and moving style tag from index.html to style.css

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import component from './component';
+// import component from './component';
 import './styles/style.css'
-import testdrawing1 from './images/testdrawing1.svg'
+// import testdrawing1 from './images/testdrawing1.svg'
 
-const img = document.createElement('img');
-img.src = testdrawing1
-document.body.appendChild(img);
+// const img = document.createElement('img');
+// img.src = testdrawing1
+// document.body.appendChild(img);
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
